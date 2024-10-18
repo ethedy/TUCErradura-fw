28e4868252c54b7e1421e6a8eb0aaf3d86293085
BrianStumpp <brian000stumpp@gmail.com>
2024-10-18 19:23:17 -0300

    Enviar Datos de MAC A RED

--- scratch/WIFI - ESP8266/ESP8266.ino
+++ scratch/WIFI - ESP8266/ESP8266.ino
@@ -1,47 +1,51 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h> 
+#include <WiFiManager.h>
+#include <WiFiClient.h>
 
-
-// Sustituir con datos de vuestra red
-const char* ssid     = "SSID";
-const char* password = "PASSWORD";
-
-ESP8266WebServer server(80);
-
-IPAddress ip(192,168,100,79);     
-IPAddress gateway(192,168,100,1);   
-IPAddress subnet(255,255,255,0);  
+ESP8266WebServer server(80); 
 
 // Pin del LED
 const int Puerta = 2;
 
-void setup()
-{
+const char* serverIp = "http://tu-servidor.com/guardar_mac"; // Cambia esto por tu servidor
+
+void setup(){
   Serial.begin(115200);
   delay(10);
   
-  WiFi.mode(WIFI_STA);
-  WiFi.config(ip, gateway, subnet);
-  WiFi.begin(ssid, password);
-  Serial.print("Conectando a:\t");
-  Serial.println(ssid); 
-
-  // Esperar a que nos conectemos
-  while (WiFi.status() != WL_CONNECTED) 
-  {
-    delay(200);
-  Serial.print('.');
-  }
+  WiFi.mode(WIFI_AP_STA);
+  WiFiManager wiFiManager;
+  wiFiManager.resetSettings();
   
-  WiFi.setAutoReconnect(true);
-
-  // Mostrar mensaje de exito y dirección IP asignada
-  Serial.println();
+  if (!wiFiManager.autoConnect("ESP8266 Config", "12345678")) {
+    Serial.println("Fallo en la conexión");
+    delay(3000);
+    ESP.restart(); // Reiniciar si la conexión falla
+  }else{
+      Serial.println();
   Serial.print("Conectado a:\t");
   Serial.println(WiFi.SSID()); 
   Serial.print("IP address:\t");
   Serial.println(WiFi.localIP());
+  Serial.print("MAC address:\t");
+  Serial.println(WiFi.macAddress());
+      }
+       // Envía la MAC a tu servidor
+    WiFiClient client;
+    if (client.connect(serverIp, 80)) {
+      client.print(String("POST /guardar_mac HTTP/1.1\r\n") +
+                   "Host: " + serverIp + "\r\n" +
+                   "Content-Type: application/x-www-form-urlencoded\r\n" +
+                   "Connection: close\r\n\r\n" +
+                   "mac=" + WiFi.macAddress());
+      delay(100); // Espera a que se envíe el mensaje
+    }
+  }
+  
 
+
+  //Definir Pin para verificar solicitud HTTP
   pinMode(Puerta, OUTPUT);
   digitalWrite(Puerta, LOW);
 
@@ -52,13 +56,21 @@ void setup()
   });
 
     server.on("/door/closed", []() {
+      
       digitalWrite(Puerta, HIGH);
       server.send(200, "text/plain", "Puerta Cerrada");
   });
 
+    server.on("/get_mac",[](){
+      String mac = WiFi.macAddress();
+      server.send(200, "text/plain", mac);
+    });
+
   server.begin();
 }
+
 void loop() 
 {
   server.handleClient();
+  
 }
