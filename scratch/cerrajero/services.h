01849aa634aa7827282331c7dffeae791beda784
Enrique Thedy <ethedy@gmail.com>
2024-11-20 21:14:50 -0300

    Agrego struct de Horario

--- scratch/cerrajero/services.h
+++ scratch/cerrajero/services.h
@@ -7,15 +7,19 @@
 //  #include "c_types.h"
 #include <cstdint>
 
-
+// TODO cambiar id por email
+// nombre de usuario es user
+// 
 /*
   struct para guardar la info de un un usuario que tiene una sesion establecida en la cerradura
 
   Renombramos 'Session' a 'UserSession' para evitar el conflicto
 */
-struct UserSession {
-  String User;
-  String PassHash;          //  Usaremos el hash de la contraseña
+struct UserSession 
+{
+  String Email;     //  correo (ID)
+  String User;      //  nombre del usuario
+  //  String PassHash;          //  Usaremos el hash de la contraseña
   String Rol;               //  "admin" o "user"
   String Token;             //  Token de sesión único para cada usuario
   uint64_t LastLogin;       //  valor de milisegundos que representa el tiempo de login, NO ES fecha/hora
@@ -26,12 +30,22 @@ struct UserSession {
   //  tal vez tengamos que agregar ctor y otros metodos
 };
 
+struct Horario {
+  int dia;    //  0 domingo
+  int hora_inicio;
+  int minuto_inicio;
+  int hora_fin;
+  int minuto_fin;
+};
+
 class SecurityServices
 {
   public:
-    UserSession login_user(const String& user_name, const String& pass);
+    UserSession login_user(const String& user_email, const String& pass);
 
-    bool add_user(const String& id, const String& user_name, const String& pass, const String& rol);
+    bool add_user(const String& user_email, const String& user_name, const String& pass, const String& rol);
+
+    bool add_horario(const String&);
 };
 
 #endif
